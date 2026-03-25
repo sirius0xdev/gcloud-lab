7cc9fcdcc29ec6cdd3c66a371d3179c6a67227ba
sirius0xdev <sirius0xdev@proton.me>
2026-03-25 00:50:09 +0000

    chamge taints for deepseek

--- modules/a3_nodepool.tf
+++ modules/a3_nodepool.tf
@@ -32,7 +32,7 @@ resource "google_container_node_pool" "h100_node_pool" {
     }
     disk_size_gb = 200
     disk_type    = "pd-ssd"
-    spot = true 
+    spot = false 
     
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"]
